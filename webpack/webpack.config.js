137d13e9a5521293c96cb44e4e032da2653bea2d
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-01-05 22:13:10 +0500

    step-9: installing and adding to config file copy-webpack-plugin for assets

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "..", "./src/index.tsx"),
@@ -35,6 +36,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "source", to: "dest" }],
     }),
   ],
 };
