f0e58e1b885d3a7080df0c8e8bdaa20c169371ec
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-01-07 23:54:19 +0500

    feat: adding props for root component to test typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import ClickCounter from "./components/ClickCounter";
-import ImageComp from "./components/ImageComp";
-import "./App.scss";
-import "./App.css";
+import ClickCounter from './components/ClickCounter';
+import ImageComp from './components/ImageComp';
+import './App.scss';
+import './App.css';
 
-const App = () => {
+const App: React.FC<{ title: string }> = ({ title }) => {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {title}</h1>
       <h2>Application running on {process.env.webpackMode} mode</h2>
       <ImageComp />
       <hr />
